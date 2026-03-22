7bb3c09e7efb49f1b7041c5361d0e3bf30cf3e5b
unknown <senin-github-email-adresin>
2026-03-22 18:37:50 +0300

    fix: correct API route for validation key

--- app/validation-key.txt/route.ts
+++ app/validation-key.txt/route.ts
@@ -1,5 +1,10 @@
 export async function GET() {
-  return new Response('ef7ae50a71c49173fb463d8010de0f9e11f9450c077605e28545faa4abaa82df66f3503220fc949f21d26a7cf87216adabaa50a6c9ca66e5922f042dcb624dea', {
-    headers: { 'Content-Type': 'text/plain' },
-  });
+  return new Response(
+    'ef7ae50a71c49173fb463d8010de0f9e11f9450c077605e28545faa4abaa82df66f3503220fc949f21d26a7cf87216adabaa50a6c9ca66e5922f042dcb624dea',
+    {
+      headers: {
+        'Content-Type': 'text/plain',
+      },
+    }
+  );
 }
